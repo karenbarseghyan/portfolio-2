4a45d9debd81fce65b343a2b74f22aec458096ce
user.email <karenbarseghyan90@gmail.com>
2020-10-26 01:59:42 +0400

    Layout of page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
+import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{height: '350px', position: 'relative'}}>
+    <Layout style={{background: '#4267B2'}}>
+        <Header transparent title="Karen Barseghyan" style={{color: 'orange'}}>
+            <Navigation>
+                <a href="#">Personal</a>
+                <a href="#">Education</a>
+                <a href="#">Experience</a>
+                <a href="#">Skills</a>
+            </Navigation>
+        </Header>
+        <Drawer title="Karen Barseghyan">
+            <Navigation>
+                <a href="#">Personal</a>
+                <a href="#">Education</a>
+                <a href="#">Experience</a>
+                <a href="#">Skills</a>
+            </Navigation>
+        </Drawer>
+        <Content />
+    </Layout>
+    <div style = {{textAlign: 'center'}}> 
+      <img src={logo} className="App-logo" alt="logo" />
     </div>
+    
+</div>
+    
   );
 }
 
 export default App;
+
