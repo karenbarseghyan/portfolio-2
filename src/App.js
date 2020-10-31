e0eeebbd633f1c023903c46d373a1c92cc1738ee
user.email <karenbarseghyan90@gmail.com>
2020-11-01 01:47:31 +0400

    Creation and Design of Skills-Page/ReactJS part

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import {Link} from 'react-router-dom';
 
 function App() {
   return (
-    <div style={{height: '350px', position: 'relative'}}>
+    <div style={{height: '360px', position: 'relative'}}>
     <Layout className = "layout-color">
         <Header transparent title="CURRICULUM VITAE" className = "header-color">
-            <Navigation style={{color: 'orange'}}>
+            <Navigation >
                 <Link to="/">Home</Link>
                 <Link to="/Personal">Personal</Link>
                 <Link to="/Education">Education</Link>
@@ -18,8 +18,8 @@ function App() {
                 <Link to="/Skills">Skills</Link>
             </Navigation>
         </Header>
-        <Drawer title="Karen Barseghyan">
-            <Navigation>
+        <Drawer title="CURRICULUM VITAE" >
+            <Navigation className = "layout-color">
                 <Link to="/">Home</Link>
                 <Link to="/Personal">Personal</Link>
                 <Link to="/Education">Education</Link>
